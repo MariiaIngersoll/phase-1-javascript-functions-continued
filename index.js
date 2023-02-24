b2990b9e34c5ead00abcf0c2c52c795e32644029
+ <+>
2023-02-23 23:12:00 -0500

    allset

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+  };
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+activity();
+
+function wrapAdjective(symbol = "*") {
+     return function msg(adj= "special") {
+        return `You are ${symbol}${adj}${symbol}!`
+     };
+}
+wrapAdjective('*') ("a dedicated programmer");
